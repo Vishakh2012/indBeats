964cd65389f87402cd3fb22f2b4fd2f1244a6ef1
[Vishwa Prakash] <[vishakpro2002@gmail.com]>
2023-01-31 14:03:15 +0530

    add change of colors on click

--- src/components/Beatmaker/Beat.js
+++ src/components/Beatmaker/Beat.js
@@ -1,20 +1,32 @@
 import  styled  from "styled-components";
+import { useState } from "react";
+export const Beat = () => {
+    const [BgColor, setBgColor] = useState('rgba(67, 207, 149, 1)');
 
-export const Beat = styled.div`
+    
+    return(
+       <Bt BgColor = {BgColor} onClick = {() => setBgColor(BgColor === 'rgba(67, 207, 149, 1)'?'rgba(0, 255, 240, 1)':'rgba(67, 207, 149, 1)')}/>
+    )
+}
+const Bt = styled.div`
     height: 50px;
     width : 50px;
-    background-color: ${({theme}) => theme.colors.beats};
+    background-color:${ ( props ) => props.BgColor } ;
     margin-right: 10px;
     :hover{
         box-shadow: 5px 5px;
     }
 `
 
-export const InstSel = styled.div`
+export const InstSelBlock = styled.div`
     background-color: blueviolet;
+    display: flex;
+    flex-direction: column;
 `
 
-
+export const InstSel = styled(Beat)`
+    background-color: ;
+`
 
 
 
