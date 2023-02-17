35965b897fb9206d9c42a755d8aab4931cb78ce9
[Vishwa Prakash] <vishakhpro2002@gmail.com>
2023-02-18 00:22:19 +0530

    trials with howler.js to figure things out

--- src/Mybeats.js
+++ src/Mybeats.js
@@ -1,7 +1,23 @@
+import {Howl} from 'howler'
 const Mybeats = () => {
+    const s = new Howl({
+        src : [ "/percussion/tabla.mp3"],
+        
+    })
+
+    const s2 = new Howl({
+        src : [ "/clap/clap1.mp3"],
+        
+    })
+    const playsound = () => {
+        s.play()
+        s2.play()
+        
+        console.log("okay");
+    }
     return ( 
         <>
-            <p>my beats</p>
+            <button onClick={() => playsound()}> play</button>
         </>
      );
 }
